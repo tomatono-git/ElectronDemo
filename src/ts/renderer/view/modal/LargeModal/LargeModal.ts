8365c23f1fbbcef91fc99fbe358bb581a8ecad82
TOMATONO-WIN10\tomat <tomatono@gmail.com>
2018-04-07 21:35:12 +0900

    コンポーネントの登録

--- src/ts/renderer/view/modal/LargeModal/LargeModal.ts
+++ src/ts/renderer/view/modal/LargeModal/LargeModal.ts
@@ -3,14 +3,30 @@ const COMPONENT_NAME = "large-modal";
 
 import * as ko from "knockout";
 // import { KnockoutComponentTypes } from "knockout";
+import { ComponentRegister } from "../../ComponentRegister";
 
 require("./LargeModal.html");
 
 export class LargeModal {
+
     private component: string;
+    // private register: ComponentRegister<this>;
 
     constructor() {
         this.component = COMPONENT_NAME;
+
+        // this.register = new ComponentRegister<this>();
+        // this.register.register(COMPONENT_NAME,
+        //     {
+        //         template: require("./LargeModal.html"),
+        //         // templatePath: "./LargeModal.html",
+        //     },
+        //     // {
+        //     //     createViewModel: (params?: any, componentInfo?) => {
+        //     //         return {} as this;
+        //     //     },
+        //     // }
+        // );
 
         if (!ko.components.isRegistered(COMPONENT_NAME)) {
             this.registerComponent(COMPONENT_NAME);
@@ -22,20 +38,20 @@ export class LargeModal {
     // }
 
     public registerComponent(componentName: string): void {
-        ko.components.register(componentName, {
-            template: require("./LargeModal.html"),
-            viewModel: {
-                createViewModel: (params?: any, componentInfo?: {
-                    element: Node;
-                    templateNodes: Node[];
-                }) => {
-                    let vm: LargeModal | undefined = undefined;
-                    if (params && params.vm) {
-                        vm = ko.unwrap(params.option);
-                    }
-                    return vm;
-                },
+        let facotry = (params?: any, componentInfo?: {
+            element: Node;
+            templateNodes: Node[];
+        }) => {
+            let vm: LargeModal | undefined = undefined;
+            if (params && params.vm) {
+                vm = ko.unwrap(params.option);
             }
+            return vm;
+        };
+        let template = require("./LargeModal.html");
+        ko.components.register(componentName, {
+            template: template,
+            viewModel: facotry,
         });
     }
 }
