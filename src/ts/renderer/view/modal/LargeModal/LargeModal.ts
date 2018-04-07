9b8a9862b98c84f740084f6de90159f9ff63db14
TOMATONO-WIN10\tomat <tomatono@gmail.com>
2018-04-07 21:36:25 +0900

    Revert "コンポーネントの登録"
    This reverts commit 8365c23f1fbbcef91fc99fbe358bb581a8ecad82.

--- src/ts/renderer/view/modal/LargeModal/LargeModal.ts
+++ src/ts/renderer/view/modal/LargeModal/LargeModal.ts
@@ -3,30 +3,14 @@ const COMPONENT_NAME = "large-modal";
 
 import * as ko from "knockout";
 // import { KnockoutComponentTypes } from "knockout";
-import { ComponentRegister } from "../../ComponentRegister";
 
 require("./LargeModal.html");
 
 export class LargeModal {
-
     private component: string;
-    // private register: ComponentRegister<this>;
 
     constructor() {
         this.component = COMPONENT_NAME;
-
-        // this.register = new ComponentRegister<this>();
-        // this.register.register(COMPONENT_NAME,
-        //     {
-        //         template: require("./LargeModal.html"),
-        //         // templatePath: "./LargeModal.html",
-        //     },
-        //     // {
-        //     //     createViewModel: (params?: any, componentInfo?) => {
-        //     //         return {} as this;
-        //     //     },
-        //     // }
-        // );
 
         if (!ko.components.isRegistered(COMPONENT_NAME)) {
             this.registerComponent(COMPONENT_NAME);
@@ -38,20 +22,20 @@ export class LargeModal {
     // }
 
     public registerComponent(componentName: string): void {
-        let facotry = (params?: any, componentInfo?: {
-            element: Node;
-            templateNodes: Node[];
-        }) => {
-            let vm: LargeModal | undefined = undefined;
-            if (params && params.vm) {
-                vm = ko.unwrap(params.option);
-            }
-            return vm;
-        };
-        let template = require("./LargeModal.html");
         ko.components.register(componentName, {
-            template: template,
-            viewModel: facotry,
+            template: require("./LargeModal.html"),
+            viewModel: {
+                createViewModel: (params?: any, componentInfo?: {
+                    element: Node;
+                    templateNodes: Node[];
+                }) => {
+                    let vm: LargeModal | undefined = undefined;
+                    if (params && params.vm) {
+                        vm = ko.unwrap(params.option);
+                    }
+                    return vm;
+                },
+            }
         });
     }
 }
